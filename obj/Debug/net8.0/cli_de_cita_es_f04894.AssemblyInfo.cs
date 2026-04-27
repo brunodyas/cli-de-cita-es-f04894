31c018ab4cf4b2d36a1544a0e900c787bad7d6ff
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 12:55:48 -0300

    feat: entrada CLI mínima
    intent: feature

--- obj/Debug/net8.0/cli_de_cita_es_f04894.AssemblyInfo.cs
+++ obj/Debug/net8.0/cli_de_cita_es_f04894.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cli_de_cita_es_f04894")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838ad69640b0a5b9287123cff48715cbd8c9fd68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e78125a4ae04629a13829b24531d08ca4f6821")]
 [assembly: System.Reflection.AssemblyProductAttribute("cli_de_cita_es_f04894")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cli_de_cita_es_f04894")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
