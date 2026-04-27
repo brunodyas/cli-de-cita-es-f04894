157c6b20dba3af199767d319439a3dfa9e956794
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 12:57:37 -0300

    feat: configuração estática
    intent: feature

--- obj/Debug/net8.0/cli_de_cita_es_f04894.AssemblyInfo.cs
+++ obj/Debug/net8.0/cli_de_cita_es_f04894.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cli_de_cita_es_f04894")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639c96cc7e823c5173c3bc14b19ca46e777b7402")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed26675cc80381ad0e2f387e85e769ab024f946")]
 [assembly: System.Reflection.AssemblyProductAttribute("cli_de_cita_es_f04894")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cli_de_cita_es_f04894")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
